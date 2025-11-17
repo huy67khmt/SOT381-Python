c905f5f345a81efce95829160802582a50280c07
huy67khmt <huy.ndg.67khmt@ntu.edu.vn>
2025-11-17 07:29:29 +0700

    lamBaiTap

---
+++ hinhChuNhat.py
@@ -0,0 +1,9 @@
+chieuDai = float(input("Nhap vao chieu dai: "))
+chieuRong = float(input("Nhap vao chieu rong: "))
+
+chuVi = (chieuDai + chieuRong) * 2
+dienTich = chieuDai * chieuRong
+
+print("chu vi cua hinh chu nhat la: ", chuVi)
+print("dien tich cua hinh chu nhat la: ", dienTich)
+
---
+++ tong2so.py
@@ -0,0 +1,5 @@
+a = float(input("Nhap vao so a:"))
+b = float(input("Nhap vao so b:"))
+x = a + b
+
+print("Tong hai so la: ", x)
