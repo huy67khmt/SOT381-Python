51b618276d3ffabfde4724b6ec31682837b28762
huy67khmt <huy.ndg.67khmt@ntu.edu.vn>
2025-11-17 07:33:52 +0700

    Create tinhBMI.py

---
+++ tinhBMI.py
@@ -0,0 +1,6 @@
+chieuCao = float(input("Nhap vao chieu cao(m): "))
+canNang = float(input("Nhap vao can nang(kg): "))
+
+BMI = canNang / (chieuCao * chieuCao)
+
+print("Chi so BMI cua ban la: ", BMI )
